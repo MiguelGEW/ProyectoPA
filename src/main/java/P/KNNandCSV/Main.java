b2c5a570cdae40108c43e0543eddc058ea38a905
Miguel Guillamón Escudero <al446461@uji.es>
2026-02-24 13:20:19 +0100

    Second Commit. Structure

--- src/main/java/P/KNNandCSV/Main.java
+++ src/main/java/P/KNNandCSV/Main.java
@@ -1,4 +1,4 @@
-package es.uji.al446461;
+package P.KNNandCSV;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
